ede5075b6f99f7dd1c3f6f22e31f4acb3b509445
Mojidul Islam <mojidul.dev.pwd@gmail.com>
2023-04-11 12:01:02 +0600

    Add project files.

---
+++ BlazorCrud/Server/Migrations/20230410093351_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorCrud.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    CityID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.CityID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    City = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Department = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Gender = table.Column<string>(type: "varchar(6)", unicode: false, maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
